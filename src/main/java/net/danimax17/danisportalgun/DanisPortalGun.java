b2bd0bf81c6bf72e904b85a53ddcd3facdb49b94
dan1max <154277102+dan1max@users.noreply.github.com>
2026-09-16 23:38:11 +0200

    Added "QTS" item

--- src/main/java/net/danimax17/danisportalgun/DanisPortalGun.java
+++ src/main/java/net/danimax17/danisportalgun/DanisPortalGun.java
@@ -1,5 +1,7 @@
 package net.danimax17.danisportalgun;
 
+import net.danimax17.danisportalgun.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -30,6 +32,8 @@ public class DanisPortalGun {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -43,7 +47,9 @@ public class DanisPortalGun {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){ // <-- Aquí el tab específico
+            event.accept(ModItems.QTS);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
